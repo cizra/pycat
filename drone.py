856900f7c5a640902e44b2bcab99180fd99252fc
Elmo Todurov <elmo.todurov@eesti.ee>
2017-05-16 12:18:48 +0300

    drone: Added

---
+++ drone.py
@@ -0,0 +1,30 @@
+import importlib
+import coffee
+importlib.reload(coffee)
+import collections
+import re
+import subprocess
+
+
+
+ALIASES = {
+        }
+TRIGGERS = {
+        'Grumpy starts ': 'play tempo',
+        'Grumpy vanishes in a puff of smoke.': 'quit;y',
+        'You hit a foul note.': '!',
+        }
+NOTIFICATIONS = {
+        }
+
+
+class Drone(coffee.Coffee):
+    def __init__(self, mud):
+        super().__init__(mud)
+        self.aliases.update(ALIASES)
+        self.triggers.update(TRIGGERS)
+        self.notifications.update(NOTIFICATIONS)
+
+
+def get_class():
+    return Drone
