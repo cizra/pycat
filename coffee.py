cae6f3991e38bc3b5daefbf0582e65239561db94
Elmo Todurov <elmo.todurov@eesti.ee>
2017-05-16 12:14:34 +0300

    coffee: Added some paths

--- coffee.py
+++ coffee.py
@@ -8,7 +8,10 @@ import time
 
 
 ALIASES = {
-        'home': 'run 6s w 2s e 2n w',
+        'home': 'run 6s w 2s e 2n;open w;w',
+        'rt vassendar': 'run 4s d w d 2w d 2n 2e;open s;s;open d;run 5d;open w;w;run 8n w 2s 6w;open w;run 11w 3n 3w;open w;run 5w;run 3n 5w',
+        'rt wgate': 'run 2s 3w;open w;w',
+        'rt sehaire': 'run w u 6w 2n 3w s 6w s 6w 2n 5w 5n w n w n 4w n e',
         }
 TRIGGERS = {
         'You feel a little cleaner, but are still very dirty.': 'bathe',
