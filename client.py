8903553fb72ec29e28c37c09a758a901f3499d31
Elmo Todurov <elmo.todurov@eesti.ee>
2017-06-05 20:27:12 +0000

    make module adding resistant to exceptions

--- client.py
+++ client.py
@@ -1,26 +1,42 @@
 import importlib
+import traceback
 import modular
 
 import modules.logging
 import modules.eval
-import modules.coffee
 import modules.repeat
+import modules.coffee
+import modules.mapper
+import modules.gaoler
 importlib.reload(modular)
 importlib.reload(modules.logging)
 importlib.reload(modules.eval)
 importlib.reload(modules.repeat)
 importlib.reload(modules.coffee)
-MODULES = [
+importlib.reload(modules.gaoler)
+importlib.reload(modules.mapper)
+MODULES = [  # determines the pecking order
         modules.eval.Eval,
         modules.repeat.Repeat,
+        modules.mapper.Mapper,
         modules.coffee.Coffee,
+        modules.gaoler.Gaoler,
         ]
 
 
 class Client(modular.ModularClient):
     def __init__(self, mud):
+        self.modules = []
+        for m in MODULES:
+            try:
+                self.modules.append(m(mud))
+            except Exception:
+                traceback.print_exc()
+        try:
+            self.modules.append(modules.logging.Logging(mud, 'new.log'))
+        except Exception:
+            traceback.print_exc()
         super().__init__(mud)
-        self.modules = [m(mud) for m in MODULES] + [modules.logging.Logging(mud, 'new.log')]
 
 
 def getClass():
