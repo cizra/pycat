ce490724d59889dbbe4a04967758b0e42f493fb8
Elmo Todurov <elmo.todurov@eesti.ee>
2017-06-05 20:27:31 +0000

    add requesting GMCP information on startup

--- pycat.py
+++ pycat.py
@@ -47,8 +47,9 @@ class Session(object):
                 sock.sendall(telnetlib.IAC + telnetlib.DO + option)
                 self.gmcpOut('Core.Hello { "client": "Cizra", "version": "1" }')
                 supportables = ['char 1', 'char.base 1', 'char.maxstats 1', 'char.status 1', 'char.statusvars 1', 'char.vitals 1', 'char.worth 1', 'comm 1', 'comm.tick 1', 'group 1', 'room 1', 'room.info 1']
-                self.log('Core.Supports.Set ' + str(supportables).replace("'", '"'))
                 self.gmcpOut('Core.Supports.Set ' + str(supportables).replace("'", '"'))
+                self.gmcpOut('request room')
+                self.gmcpOut('request char')
             elif option == telnetlib.TTYPE:
                 log("Sending terminal type 'Cizra'")
                 sock.sendall(telnetlib.IAC + telnetlib.DO + option +
@@ -131,6 +132,8 @@ class Session(object):
                 self.world_module = importlib.reload(self.world_module)
                 self.world = self.world_module.getClass()(self)
                 self.world.state = state
+                self.gmcpOut('request room')
+                self.gmcpOut('request char')
             except Exception:
                 traceback.print_exc()
             return
