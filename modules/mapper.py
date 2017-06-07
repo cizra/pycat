cae699676f0e151470c46af717d4686b1607a465
Elmo Todurov <elmo.todurov@eesti.ee>
2017-06-07 21:21:46 +0000

    add bookmarks functionality to the mapper

--- modules/mapper.py
+++ modules/mapper.py
@@ -3,64 +3,115 @@ import mapper.libmapper
 import pprint
 import re
 import time
+import json
 
 
 class Mapper(BaseModule):
+    def help(self, args):
+        strs = ["Commands:"]
+        for cmd in self.commands.keys():
+            strs.append(cmd)
+        self.log('\n'.join(strs))
+
+    def current(self):
+        return self.gmcp['room']['info']['num']
+
+    def here(self, args):
+        if args:
+            this = int(args[0])
+        else:
+            this = self.current()
+        self.log('\n' + pprint.pformat({
+            'num': this,
+            'name': self.m.getRoomName(this),
+            'data': self.m.getRoomData(this),
+            'coords': self.m.getRoomCoords(this),
+            'exits': self.m.getRoomExits(this),
+            }))
+
+    def path(self, args):
+        there = args[0]
+        if there in self.data['bookmarks']:
+            there = self.data['bookmarks'][there]
+        else:
+            there = int(there)
+
+        this = self.current()
+        if this == there:
+            self.log("Already there!")
+            return ''
+        then = time.time()
+        path = self.m.findPath(this, there)
+        self.log("{} (found in {} seconds)".format(path, time.time() - then))
+        return path
+
+    def go(self, args):
+        self.send(self.path(args))
+
+    def bookmarks(self, args):
+        self.log('Bookmarks:\n' + pprint.pformat(self.data['bookmarks']))
+
+    def bookmark(self, args):
+        arg = ' '.join(args)
+        self.data['bookmarks'][arg] = self.current()
+        self.bookmarks([])
+
+    def draw(self):
+        self.log("""
+█ █ █
+│ │ │
+▒─▒╫▒╸
+│ │ │
+▓ ▓ ▓
+""")
+
+    def quit(self):
+        self.m.setMapData(json.dumps(self.data))
+        with open(self.mapfname, 'w') as f:
+            f.write(self.m.serialize())
+        self.log("Serialized map to ", self.mapfname)
+
     def __init__(self, mud, mapfname='default.map'):
         self.mapfname = mapfname
         try:
             with open(self.mapfname, 'r') as f:
                 ser = f.read()
             self.m = mapper.libmapper.Map(ser)
+            self.data = json.loads(self.m.getMapData())
         except FileNotFoundError:
+            self.data = {
+                    'bookmarks': {},
+                    }
             self.m = mapper.libmapper.Map()
+
+        self.commands = {
+                'help': self.help,
+                'here': self.here,
+                'bookmark': self.bookmark,
+                'bookmarks': self.bookmarks,
+                'path': self.path,
+                'go': self.go,
+                'save': lambda: self.quit([])
+                }
+
         super().__init__(mud)
-
-    def quit(self):
-        with open(self.mapfname, 'w') as f:
-            f.write(self.m.serialize())
-        self.log("Serialized map to ", self.mapfname)
-
-    def current(self):
-        return self.gmcp['room']['info']['num']
-
-    def path(self, there):
-        here = self.current()
-        if here == there:
-            self.log("Already there!")
-            return ''
-        then = time.time()
-        path = self.m.findPath(here, there)
-        self.log("{} (found in {} seconds)".format(path, time.time() - then))
-        return path
 
     def alias(self, line):
         words = line.split(' ')
+
         if words[0] != '#map':
             return
-        cmd = words[1]
-        if cmd == 'here':
-            here = self.current()
-            self.log('\n' + pprint.pformat({
-                'num': here,
-                'name': self.m.getRoomName(here),
-                'zone': self.m.getRoomZone(here),
-                'terrain': self.m.getRoomTerrain(here),
-                'coords': self.m.getRoomCoords(here),
-                'exits': self.m.getRoomExits(here),
-                }))
-            return True
-        elif re.match(r'#map path ([^ ]+)', line):
-            there = int(re.match(r'#map path ([^ ]+)', line).group(1))
-            self.log(self.path(there))
-            return True
-        elif re.match(r'#map go ([^ ])', line):
-            there = int(re.match(r'#map path ([^ ]+)', line).group(1))
-            self.send(self.path(there))
+
+        if len(words) == 1:
+            self.draw()
             return True
 
-    def trigger(self, raw, stripped):
-        pass
+        cmd = words[1]
+        if cmd in self.commands:
+            self.commands[cmd](words[2:])
+        else:
+            self.help(words[2:])
+        return True
 
     def handleGmcp(self, cmd, value):
         # room.info {'details': '', 'id': 'Homes#1226', 'terrain': 'cave', 'exits': {'N': -565511209}, 'coord': {'id': 0, 'x': -1, 'cont': 0, 'y': -1}, 'desc': '', 'num': -565511180, 'name': 'An empty room', 'zone': 'Homes'}
@@ -76,9 +127,8 @@ class Mapper(BaseModule):
         if cmd == 'room.info':
             id = value['num']
             name = value['name']
-            zone = value['zone']
-            terrain = value['terrain']
+            data = dict(zone=value['zone'], terrain = value['terrain'])
             exits = {}
             for k, v in value['exits'].items():
                 exits[k.lower()] = v
-            self.m.addRoom(id, name, zone, terrain, exits)
+            self.m.addRoom(id, name, json.dumps(data), exits)
