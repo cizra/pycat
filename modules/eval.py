207907532f5986b69816552bdb58f479fb756a10
Elmo Todurov <elmo.todurov@eesti.ee>
2017-10-16 12:49:15 +0000

    Eval: pretty output

--- modules/eval.py
+++ modules/eval.py
@@ -1,11 +1,12 @@
 from modules.basemodule import BaseModule
+from pprint import pformat
 
 
 class Eval(BaseModule):
     def alias(self, line):
         if line.startswith('#py '):
             rest = line[4:]
-            self.mud.log(repr(eval(rest)))
+            self.mud.log("\n" + pformat(eval(rest)))
             return True
         elif line.startswith('#pye '):
             rest = line[5:]
