bbf52f1b59b315108f685a371f1f18fa3c15dbe9
Elmo Todurov <elmo.todurov@eesti.ee>
2018-11-03 22:21:46 +0000

    Changed proxy to kick out old connection when new one connects

--- proxy.py
+++ proxy.py
@@ -7,23 +7,27 @@ import socketserver
 import threading
 import time
 
-TGT_HOST = 'coffeemud.net'
-TGT_PORT = 2323
+TGT_HOST = 'localhost'
+TGT_PORT = 4000
 LISTEN_HOST = '::1'
-LISTEN_PORT = 4000
+LISTEN_PORT = 1234
 
 eof = object()
 # to server
-to_q = queue.Queue()
+client_to_mud_q = queue.Queue()
 # from server
-from_q = queue.Queue()
-global connected
-connected = False
+mud_to_client_q = queue.Queue()
+mudside_connected = False
+client_sessions = []
 
 
-class MyTCPServer(socketserver.TCPServer):
+class MyTCPServer(socketserver.ThreadingTCPServer):
     address_family = socket.AF_INET6
     allow_reuse_address = True
+
+
+def log(arg):
+    print("{} {}".format(threading.get_ident(), arg))
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -37,32 +41,41 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     request_queue_size = 1
     address_family = socket.AF_INET6
 
-    def my_send(self):
-        print("Starting my_send")
-        while True:
-            data = from_q.get(block=True)
-            if data is eof:
-                break
-            else:
-                self.request.sendall(data)
-        print("Ending my_send")
+    def my_send(self, shutdown1):
+        log("Starting my_send")
+        while not shutdown1.is_set():
+            data = mud_to_client_q.get(block=True)
+            self.request.sendall(data)
+        shutdown1.clear()
+        log("Ending my_send")
 
-    def my_receive(self):
-        print("starting my_receive")
-        while True:
+    def my_receive(self, shutdown2):
+        log("starting my_receive")
+        while not shutdown2.is_set():
             data = self.request.recv(1024)
             if not data:
                 break
-            print("{} wrote {} bytes".format(self.client_address[0], len(data)))
-            print(data)
-            to_q.put(data)
-        from_q.put(eof)
-        print("Ending my_receive")
+            # print("{} wrote {} bytes".format(self.client_address[0], len(data)))
+            # print(data)
+            client_to_mud_q.put(data)
+        shutdown2.clear()
+        log("Ending my_receive")
 
     def handle(self):
         print("Incoming client connection")
-        self.send_thr = threading.Thread(target=self.my_send)
-        self.recv_thr = threading.Thread(target=self.my_receive)
+
+        for event in client_sessions:
+            print("Kicking out old client")
+            event.set()
+        del client_sessions[:]
+        
+        shutdown1 = threading.Event()
+        shutdown2 = threading.Event()
+        client_sessions.append(shutdown1)
+        client_sessions.append(shutdown2)
+
+        self.send_thr = threading.Thread(target=self.my_send, args=[shutdown1])
+        self.recv_thr = threading.Thread(target=self.my_receive, args=[shutdown2])
         self.send_thr.start()
         self.recv_thr.start()
         self.send_thr.join()
@@ -74,37 +87,37 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
 
 # MUD client, talking to server
-def cli_recv(sock):
-    print("cli_recv started")
+def mud_to_client_threadfn(mud_sock):
+    print("mud_to_client_threadfn started")
     while True:
-        data = sock.recv(1024)
+        data = mud_sock.recv(1024)
         if not data:
             break
-        print("Got data from server, %d bytes" % len(data))
-        from_q.put(data)
-    global connected
-    connected = False
-    to_q.put(eof)
-    print("cli_recv ended")
+        # print("Got data from server, %d bytes" % len(data))
+        mud_to_client_q.put(data)
+    global mudside_connected
+    mudside_connected = False
+    client_to_mud_q.put(eof)
+    print("mud_to_client_threadfn ended")
 
 
-def cli_send(sock):
-    print("cli_send started")
+def client_to_mud_threadfn(mud_sock):
+    print("client_to_mud_threadfn started")
     while True:
-        data = to_q.get(block=True)
+        data = client_to_mud_q.get(block=True)
         if data is eof:
             break
         else:
-            sock.sendall(data)
-    print("cli_send ended")
+            mud_sock.sendall(data)
+    print("client_to_mud_threadfn ended")
 
 
 def keepalive():
-    global connected
+    global mudside_connected
     while True:
-        if connected:
+        if mudside_connected:
             print("*keepalive*")
-            to_q.put(b"\r")
+            client_to_mud_q.put(b"\r")
         time.sleep(random.randint(101,299))
 
 
@@ -120,22 +133,22 @@ if __name__ == "__main__":
     ser_thr = threading.Thread(target=server.serve_forever)
     ser_thr.start()
 
-    while True:
-        print("Started outgoing connection to %s:%d" % (TGT_HOST, TGT_PORT))
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            sock.connect((TGT_HOST, TGT_PORT))
-            connected = True
-        except:
-            pass
-        else:
-            print("Outgoing connected")
-            cli_recv_thr = threading.Thread(target=cli_recv, args=[sock])
-            cli_send_thr = threading.Thread(target=cli_send, args=[sock])
-            cli_recv_thr.start()
-            cli_send_thr.start()
-            cli_recv_thr.join()
-            cli_send_thr.join()
-        time.sleep(1)
+    # while True:
+    print("Starting mudside connection to %s:%d" % (TGT_HOST, TGT_PORT))
+    mud_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        mud_sock.connect((TGT_HOST, TGT_PORT))
+        mudside_connected = True
+    except:
+        pass
+    else:
+        print("Outgoing mudside_connected")
+        cli_recv_thr = threading.Thread(target=mud_to_client_threadfn, args=[mud_sock])
+        cli_send_thr = threading.Thread(target=client_to_mud_threadfn, args=[mud_sock])
+        cli_recv_thr.start()
+        cli_send_thr.start()
+        cli_recv_thr.join()
+        cli_send_thr.join()
+    # time.sleep(1)
 
     ser_thr.join()
