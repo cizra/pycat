ff975916af6d344681b3d1e5b49b447ff624ad54
Ivan Drobyshevskyi <drobyshevskyi@gmail.com>
2019-09-22 10:19:58 +0300

    Add tmux wrapper for pycat + rlwrap

--- start_client.sh
+++ start_client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-LD_PRELOAD=libboost_serialization.so:libboost_python37.so:libpython3.7m.so::libstdc++.so.6:libmapper.so rlwrap ./pycat.py "${MODULE:-client}" "${ARG}"
+LD_PRELOAD=libboost_serialization.so:libboost_python37.so:libpython3.7m.so::libstdc++.so.6:libmapper.so ./pycat.py "${MODULE:-coffee}" "${ARG}"
