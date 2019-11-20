17148a806fc02417eaf8f90f31212576eda022ae
Elmo Todurov <elmo.todurov@eesti.ee>
2019-11-20 18:57:32 +0000

    Added sample

---
+++ sample.py
@@ -0,0 +1,50 @@
+import importlib
+import traceback
+
+import modular
+import modules.logging
+import modules.eval
+import modules.repeat
+import modules.mapper
+importlib.reload(modular)
+importlib.reload(modules.logging)
+importlib.reload(modules.eval)
+importlib.reload(modules.repeat)
+importlib.reload(modules.mapper)
+
+
+class Sample(modular.ModularClient):
+    def __init__(self, mud, name):
+
+        self.name = name
+        self.logfname = '{}.log'.format(name)
+        self.mapfname = 'sample.map'.format(name)
+
+        self.modules = {}
+        mods = {
+                'eval': (modules.eval.Eval, []),
+                'repeat': (modules.repeat.Repeat, []),
+                'logging': (modules.logging.Logging, [self.logfname]),
+                'mapper': (modules.mapper.Mapper, [True, self.mapfname, True]),
+                }
+
+        for modname, module in mods.items():
+            try:
+                constructor, args = module
+                args = [mud] + args
+                self.modules[modname] = constructor(*args)
+            except Exception:
+                traceback.print_exc()
+
+        super().__init__(mud)
+
+        self.aliases.update(ALIASES)
+        self.triggers.update(TRIGGERS)
+
+
+
+    def getHostPort(self):
+        return '172.17.0.1', 7902
+
+def getClass():
+    return Sample
