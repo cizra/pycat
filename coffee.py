8125c1153ac29bf5782da51274e6e85fada0b71c
Elmo Todurov <elmo.todurov@eesti.ee>
2019-12-22 12:43:48 +0200

    coffee: speedwalks

--- coffee.py
+++ coffee.py
@@ -55,10 +55,12 @@ def showHones(mud, _):
 
 
 ALIASES = {
-        'home': 'run 6s w 2s e 2n\nopen w\nw',
+        'home': 'run 6s w 2s e 2n\nunlock w\nopen w\nw\nlock e',
         'rt vassendar': 'run 4s d w d 2w d 2n 2e\nopen s\ns\nopen d\nrun 5d\nopen w\nw\nrun 8n w 2s 6w\nopen w\nrun 11w 3n 3w\nopen w\nrun 5w\nrun 3n 5w',
         'rt wgate': 'run 2s 3w\nopen w\nw',
         'rt sehaire': 'run w u 6w 2n 3w s 6w s 6w 2n 5w 5n w n w n 4w n e',
+        'rt magic-forest': 'go 2S  3w\n open w\n go 2W  U  W  S  6W  9N W  U  N  E  N  W  2D  N  E  3N  W  5N  W  N  W  N  4W  N  E',
+        'rt sengalion': 'go 2S  3w\n open w\n go 2w U  6W  2N  3W  S  6W  S  3W  7S  6E  S',
         '#hone (.+)': hone,
         '#hones': showHones,
         }
