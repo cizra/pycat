d30ae6f845c84c54efc6b97cfd723e2eb09fb233
Elmo Todurov <elmo.todurov@eesti.ee>
2020-07-18 20:16:36 +0000

    fix gzlogging

--- modules/gzlogging.py
+++ modules/gzlogging.py
@@ -7,7 +7,7 @@ import subprocess
 class GzLogging(BaseModule):
     def __init__(self, mud, logfname):
         self.logfname = logfname
-        self.file = gzip.open(logfname, 'a', buffering=1)
+        self.file = gzip.open(logfname, 'at')
         super().__init__(mud)
 
     def quit(self):
@@ -16,7 +16,7 @@ class GzLogging(BaseModule):
     def alias(self, line):
         if line.startswith('#grep '):
             arg = line[len('#grep '):]
-            grep = subprocess.Popen(['/bin/sh', '-c', 'tail -n10000 {} | grep -a {}'.format(self.logfname, arg)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            grep = subprocess.Popen(['/bin/sh', '-c', 'tail -n10000 {} | zgrep -a {}'.format(self.logfname, arg)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = grep.communicate(timeout=5)
             self.mud.log('\n' + out.decode('utf-8'))
             return True
