ad1ce6e6116667c7951f01b17c054d496582f2ca
Elmo Todurov <etodurov@microsoft.com>
2021-04-01 13:52:44 +0300

    remove junk from mage. Add triggers for meditation

--- modules/mage.py
+++ modules/mage.py
@@ -4,72 +4,6 @@ import random
 import re
 import time
 
-
-# start with at least 10 charisma, max int and wis
-# At level 1, train scholar, practice write
-# At level 2, write book
-# At level 3, gain engraving and embroidering, prac herbology
-# At level 4, gain combat log
-#(# At level 5, gain organizing)
-# At level 7, gain find home
-# At level 9, gain wilderness lore
-# At level 11, gain book naming
-# At level 13, gain find ship
-# At level 14, gain make maps
-# At level 16, gain plant lore
-# At level 22, gain planar lore
-# At level 30, gain master herbology
-
-spells_by_level = {
-        1: ['herb herb'],
-        2: ['label book', 'engrave bowl bowl', 'embroider belt belt'],
-        3: ['light fire', 'combatlog self', 'combatlog stop'],
-        4: ['organ room name'],
-        # 5: ['smokesig testing testing 1 2 3'],
-        6: ['entitle book book', 'findhome'],
-        7: ['bookedit book', 'knowplant herb'],
-        9: ['wlore', 'transcribe paper book'],
-        10: ['bname book 1 book', 'speculate'],
-        11: ['lore book'],
-        12: ['rlore human', 'findship'],
-        13: ['cwrite book', 'map paper'],
-        15: ['plantlore'],
-        18: ['recollect happy'],
-        21: ['plore astral'],
-        22: ['survey room book'],
-        29: ['mherb herb'],
-        }
-
-def practiceOne(mud):
-    level = mud.level() - 1
-    mud.log("practiceOne")
-    practiceImpl(mud, level, 9, -1)
-
-def practiceTwo(mud):
-    mud.log("practiceTwo")
-    practiceImpl(mud, 9, 0, -1)
-
-def practiceImpl(mud, begin, end, step):
-    if mud.gmcp['room']['info']['num'] != 1741703288:
-        mud.log("Not running Scholar script - must be in Pecking Place")
-        return
-
-    out = []
-    for i in range(begin, end, step):
-        if i in spells_by_level:
-            for skill in spells_by_level[i]:
-                out.append(skill)
-
-    # don't wake when too low-level to actually do anything
-    if out:
-        mud.send("sta")
-        for elem in out:
-            mud.send(elem)
-        mud.send("sleep")
-
-    return
-
-
 class Mage(BaseModule):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -88,7 +22,7 @@ class Mage(BaseModule):
         if 'wantSpells' not in self.world.state:
             self.world.state['wantSpells'] = set()
         self.world.state['wantSpells'].add(spell)
-        cast(spell)
+        self.cast(spell)
         self.log(spell + " wore off")
 
     def wearOn(self, spell):
@@ -116,12 +50,11 @@ class Mage(BaseModule):
         self.send('stand')
         self.spellUp()
 
-        if self.world.gmcp['room']['info']['num'] != 1741703288:
-            self.log("Not running Mage script - must be in Grumpy Grotto")
-            return
 
     def getTriggers(self):
         return {
+                'Your meditation is interrupted by the noise.': 'meditate',
+                'You attempt to meditate, but lose concentration.': 'meditate',
                 'You speak quickly to yourself, but nothing more happens.': lambda mud, groups: self.spellFail('haste'),
                 'You attempt to invoke magical protection, but fail.': lambda mud, groups: self.spellFail('mage armor'),
                 'You attempt to invoke an anti-magic globe, but fail.': lambda mud, groups: self.spellFail('globe'),
