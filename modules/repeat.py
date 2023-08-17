13df50e0f7e0c5b0ddf51417ad4e219f119a30e7
Elmo Todurov <elmo_git@todurov.eu>
2023-08-17 13:21:44 +0300

    Add a way to send newlines through repeat module

--- modules/repeat.py
+++ modules/repeat.py
@@ -10,5 +10,7 @@ class Repeat(BaseModule):
     def alias(self, line):
         if line == '':
             self.mud.send(self.outlog[0])
+        elif line == '#nl':
+            self.mud.send('')
         else:
             self.outlog.appendleft(line)
